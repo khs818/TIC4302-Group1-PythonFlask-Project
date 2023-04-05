de5c2b58f24b1294c3aaf5fb45a4f17932533b52
khs818 <jetman51888@gmail.com>
2023-04-05 14:59:53 +0800

    fix code format

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,7 +1,6 @@
 import os
-
-from flask import Flask
 import hashlib
+from flask import Flask
 
 def create_app(test_config=None):
     """Create and configure an instance of the Flask application."""
